b1fd4481fdcecfc4ca7441367b474de1bb3a3ac4
nikolas52 <mnimilgwm123@wp.pl>
2025-05-23 16:11:48 +0200

    Dodanie CRUD'a oraz frontendu

---
+++ FlyEasy/Models/Flight.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlyEasy.Models
+{
+    public class Flight
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonIgnoreIfNull] 
+        public string? Id { get; set; } 
+
+        [Required(ErrorMessage = "Numer lotu jest wymagany")]
+        public string FlightNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Miejsce wylotu jest wymagane")]
+        public string Departure { get; set; } = null!;
+
+        [Required(ErrorMessage = "Destynacja jest wymagana")]
+        public string Destination { get; set; } = null!;
+
+        [Required(ErrorMessage = "Czas odlotu jest wymagany")]
+        public DateTime DepartureTime { get; set; }
+
+        public List<Passenger> Passengers { get; set; } = new List<Passenger>();
+        public List<Luggage> Luggage { get; set; } = new List<Luggage>();
+    }
+
+    public class Passenger
+    {
+        public string Name { get; set; } = null!;
+        public string PassportNumber { get; set; } = null!;
+        public string SeatNumber { get; set; } = null!;
+    }
+
+    public class Luggage
+    {
+        public string LuggageId { get; set; } = null!;
+        public double Weight { get; set; }
+        public string Type { get; set; } = null!;
+    }
+}
--- FlyEasy/Program.cs
+++ FlyEasy/Program.cs
@@ -1,27 +1,22 @@
+using Microsoft.Extensions.Options;
+using FlyEasy.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<FlightContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
